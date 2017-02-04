ec894c4103cb776ebdd4fbd47e2b941c9ebd3774
Stoliarov Oleg <stoljarov.oleg@gmail.com>
2017-02-05 00:45:37 +0200

    Add Java config for MVC (custom jsp viewResolver) and Hibernate

---
+++ src/main/java/com/stolser/config/RootConfig.java
@@ -0,0 +1,83 @@
+package com.stolser.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = {"com.stolser"})
+@EnableJpaRepositories(basePackages = {"com.stolser.repository"})
+@PropertySource("classpath:application.properties")
+@EnableTransactionManagement(mode = AdviceMode.PROXY)
+public class RootConfig {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(env.getRequiredProperty("db.driver"));
+        dataSource.setUrl(env.getRequiredProperty("db.url"));
+        dataSource.setUsername(env.getRequiredProperty("db.username"));
+        dataSource.setPassword(env.getRequiredProperty("db.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource,
+                                                                       JpaVendorAdapter vendorAdapter) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
+        entityManagerFactoryBean.setPackagesToScan(env.getRequiredProperty("entitymanager.packages.to.scan"));
+//        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistence.class);
+//        entityManagerFactoryBean.setJpaProperties(hibProperties());
+
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+//        adapter.setDatabasePlatform();
+        return adapter;
+    }
+
+//    @Bean
+//    public HibernateTransactionManager transactionManager() {
+//        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+//        transactionManager.setSessionFactory(sessionFactory(dataSource()).getObject());
+//        return transactionManager;
+//    }
+//
+//    @Bean
+//    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+//        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+//        sessionFactoryBean.setDataSource(dataSource);
+//        sessionFactoryBean.setPackagesToScan(env.getRequiredProperty("entitymanager.packages.to.scan"));
+//        sessionFactoryBean.setHibernateProperties(hibProperties());
+//        return sessionFactoryBean;
+//    }
+//
+//    private Properties hibProperties() {
+//        Properties properties = new Properties();
+//        properties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+//        properties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+//        properties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+//        properties.put("hibernate.connection.autocommit", env.getRequiredProperty("hibernate.connection.autocommit"));
+//        return properties;
+//    }
+}
